fa749b3478fbdc2eb92a6fec33ea7060150c798e
As1th <asithumavin@gmail.com>
2025-02-10 13:38:28 +0530

    Anim

--- Assets/Script/DragGameSprite.cs
+++ Assets/Script/DragGameSprite.cs
@@ -288,7 +288,7 @@ public class DragGameSprite : MonoBehaviour
 
                 while (Vector3.Distance(transform.position, targetPos) > 0.05f)
                 {
-                    if (isDragging || rotateMode || (Menu != null && Menu.activeSelf) || animator.GetCurrentAnimatorStateInfo(0).IsName("Joy") || animator.GetCurrentAnimatorStateInfo(0).IsName("Pet"))
+                    if (isDragging || rotateMode || (Menu != null && Menu.activeSelf) || animator.GetCurrentAnimatorStateInfo(0).IsName("Joy") || animator.GetCurrentAnimatorStateInfo(0).IsName(""))
                     {
                         animator.SetBool("IsWalking", false);
                         break;
